03958a3bbfbb89482c299c3da8946d16131d2b2d
Kalin Jack <kalinj1997@gmail.com>
2021-01-05 16:55:45 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,25 +8,14 @@ end
 
 # code your input_to_index and move method here!
 
-it 'converts a user_input to an integer' do
-  user_input = "1"
-  converted_input = input_to_index(user_input)
 
-  expect(converted_input).to be_a(Integer)
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
 end
 
-it 'subtracts 1 from the user_input' do
-  user_input = "6"
-  converted_input = input_to_index(user_input)
-
-  expect(converted_input).to be(5)
-end
-
-it 'returns -1 for strings without integers' do
-  user_input = "invalid"
-  converted_input = input_to_index(user_input)
-
-  expect(converted_input).to be(-1)
-end
-
+def move(board, index, character = "X")
+  board[index] = character
+  return board
 end
